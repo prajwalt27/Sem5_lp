0f07a09b79592942efe996fc7a4e6a6f84388983
Prajwal Toundakar <81010237+prajwalt27@users.noreply.github.com>
2022-07-21 16:23:07 +0530

    Update B2.java

--- B2.java
+++ B2.java
@@ -11,9 +11,12 @@ public class second {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 //         System.out.println("ok go ahead");
+		
          Scanner s= new Scanner(System.in);
          System.out.println("\n Enter Total FCFS Values: ");
          
+         float avg_waitingtime=0;
+         float avg_tat_time=0;
          int n= s.nextInt();
          int arr[]= new int[n];
          int arr1[]= new int[n];
@@ -35,6 +38,7 @@ public class second {
          System.out.println("Tw values: ");
          for(int i=0;i<n;i++)
          {
+        	 avg_waitingtime+=arr1[i];
         	 System.out.println("Tw("+(i+1)+"): "+arr1[i]);
          }
          System.out.println("\n The TAT values are: ");
@@ -42,9 +46,58 @@ public class second {
          for(int i=0;i<n;i++)
          {          	 
         	 System.out.println("\n TAT("+(i+1)+")"+" : "+(arr[i]+2*arr1[i]));
+        	 avg_tat_time+=(arr[i]+2*arr1[i]);
          }
+         System.out.println("\n The avg. Waiting Time is: "+(avg_waitingtime/n));
+         System.out.println("\n The avg. turn around time is: "+(avg_tat_time/n));
          
 
 	}
 
 }
+
+//OUTPUT
+// Enter Total FCFS Values: 
+// 5
+
+//  Enter TS values: 
+
+//  For P1 ,Ts1 = 
+// 300
+
+
+//  For P2 ,Ts2 = 
+// 125
+
+//  For P3 ,Ts3 = 
+// 400
+
+//  For P4 ,Ts4 = 
+// 150
+
+//  For P5 ,Ts5 = 
+// 100
+
+
+// Tw values: 
+// Tw(1): 0
+// Tw(2): 300
+// Tw(3): 425
+// Tw(4): 825
+// Tw(5): 975
+
+//  The TAT values are: 
+
+//  TAT(1) : 300
+
+//  TAT(2) : 725
+
+//  TAT(3) : 1250
+
+//  TAT(4) : 1800
+
+//  TAT(5) : 2050
+
+//  The avg. Waiting Time is: 505.0
+
+//  The avg. turn around time is: 1225.0
