67d912f6fb58aef1176bd48857964c6eeaff32d0
Prajwal Toundakar <81010237+prajwalt27@users.noreply.github.com>
2022-11-15 15:27:44 +0530

    Update mongoconnect.java

--- DBMS-LAB/Mock/mongoconnect.java
+++ DBMS-LAB/Mock/mongoconnect.java
@@ -19,17 +19,17 @@ public class mongoconnect {
 		try {
 			MongoClient mongo=new MongoClient("localhost",27017);
 			
-			MongoDatabase db= mongo.getDatabase("mock");
+			MongoDatabase db= mongo.getDatabase("testDB");
 			
 			MongoCollection coll= db.getCollection("jdbc");
 			
 			do {
 				System.out.println("*** Menu ***\n"+
-				  "1. Insert \n"+
+			    "1. Insert \n"+
 			    "2. Update \n"+
 			    "3. Delete \n"+
 			    "4. Display \n"+
-		      "5. Exit");
+		            "5. Exit");
 				System.out.println("Enter your choice: ");
 				choice= sc.nextInt();
 				
@@ -51,25 +51,26 @@ public class mongoconnect {
 					dept= sc.nextLine();
 					d.put("dept",dept);
 										
-	     			coll.insertOne(d);
+	     			        coll.insertOne(d);
 					break;
 				
 				case 2:
 					Document u= new Document();
 					
-					System.out.println("Enter roll no. which you to update: ");
-					value=sc.nextLine();
+					System.out.println("Enter roll no. of entry which you want to update: ");
+					value=sc.next();
 					u.put("rollno",value);
 					
 					Document d1= new Document();
+//					Strivalue= sc.nextLine();
 					d1.put("rollno", value);
 					
 					System.out.println("Enter name: ");
-					name=sc.nextLine();
+					name=sc.next();
 					d1.put("name",name);
 					
 					System.out.println("Enter dept: ");
-					dept=sc.nextLine();
+					dept=sc.next();
 					d1.put("dept", dept);
 					
 					coll.replaceOne(u,d1);
@@ -77,11 +78,11 @@ public class mongoconnect {
 				
 				case 3:
 					System.out.println("Enter rollno want to delete: ");
-					value= sc.nextLine();
+					value= sc.next();
 					Document r= new Document();
 					
-					r.put("roll",value);
-					coll.deleteOne(r);
+					r.put("rollno",value);
+					coll.deleteMany(r);
 					break;
 					
 				case 4:
@@ -101,12 +102,11 @@ public class mongoconnect {
 					System.exit(0);
 					break;    
 			 }
-			} while(choice!=5);
+			}while(choice!=5);
+		}catch(MongoException e) {
+			e.printStackTrace();
 		}
-     catch(MongoException e) 
-     {
-			 e.printStackTrace();
-		 }
+
 	}
 
 }
